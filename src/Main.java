477294fc77e21d99974589d868c82ff92025383b
Galina Shuravina <shuravinags@mail.ru>
2022-06-11 13:49:25 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int eaters = 5;
+
+        int water = 3000;
+        int potatoes = 5;
+        int chicken = 6;
+        int spices = 10;
+
+        System.out.printf("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + "миллилитров воды");
+        System.out.println((potatoes / eaters) + "картофелин(а)");
+        System.out.println((chicken / eaters) + "куриных(ое) бедер(ро)");
+        System.out.println((spices / eaters) + "ложек(ка) специй");
+    }
+
+}
